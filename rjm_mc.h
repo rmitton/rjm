5e3fd2bb9396e9cad2fb7615ba372ba9d8a4f85f
Richard Mitton <rmitton@users.noreply.github.com>
2018-01-12 10:38:55 -0800

    Added full example
    Added a complete working example to show off how to use it.

--- rjm_mc.h
+++ rjm_mc.h
@@ -4,10 +4,8 @@
 // To generate the implementation, place this define in exactly one source
 // file before including the header:
 // #define MC_IMPLEMENTATION
-//
-// To use, call mcGenerate.
 
-
+//--- Licence -------------------------------------------------------------
 // This is free and unencumbered software released into the public domain.
 // 
 // Anyone is free to copy, modify, publish, use, compile, sell, or
@@ -33,6 +31,45 @@
 // 
 // For more information, please refer to <http://unlicense.org/>
 
+//--- A complete usage example --------------------------------------------
+//
+//     #include <stdio.h>
+//     #define MC_IMPLEMENTATION
+//     #include "rjm_mc.h"
+//     float testIsoFn(const float *pos, float *extra, void *userparam)
+//     {
+//         // A simple unit sphere at the origin.
+//         float x = pos[0], y = pos[1], z = pos[2];
+//         return (x*x + y*y + z*z) - 1;
+//     }
+//     
+//     int main()
+//     {
+//         float bmin[3] = { -2, -2, -2 }; // bounding-box to scan over
+//         float bmax[3] = { +2, +2, +2 };
+//         float res = 0.1f;               // step size
+//         
+//         McMesh mesh = mcGenerate(bmin, bmax, res, testIsoFn, NULL);
+//         
+//         // Save out as a .OBJ 3D model.
+//         printf("o isosurface\n");
+//         for (int n=0;n<mesh.nverts;n++)
+//             printf("v %f %f %f\n", mesh.verts[n].x, mesh.verts[n].y, mesh.verts[n].z);
+//         for (int n=0;n<mesh.nverts;n++)
+//             printf("vn %f %f %f\n", mesh.verts[n].nx, mesh.verts[n].ny, mesh.verts[n].nz);
+//         for (int n=0;n<mesh.ntris;n++) {
+//             int a = mesh.indices[n*3+0] + 1;  // +1 because .OBJ counts from 1
+//             int b = mesh.indices[n*3+1] + 1;
+//             int c = mesh.indices[n*3+2] + 1;
+//             printf("f %i//%i %i//%i %i//%i\n", a,a, b,b, c,c);
+//         }
+//         
+//         mcFree(&mesh); // Free it when done.
+//         return 0;
+//     }
+//
+
+//--- Header/API ----------------------------------------------------------
 #ifndef __RJM_MC_H__
 #define __RJM_MC_H__
 
